0a38b2d6c25ce2d535759a4b77951935996e21cf
Satpal Kaler <87140988+SatpalKaler@users.noreply.github.com>
2024-12-05 19:05:00 +0800

    Update App.vue

--- retirementcalcVue/src/App.vue
+++ retirementcalcVue/src/App.vue
@@ -1,12 +1,10 @@
 <template>
   <v-app>
     <v-main>
-      <Analytics />
       <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script setup>
-import { Analytics } from "@vercel/analytics/react"
 </script>
